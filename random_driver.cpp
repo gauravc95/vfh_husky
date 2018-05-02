f1e6ec3189761a2d5cd2acb80f2d4261027b1c52
Gaurav Chaudhari <c.gaurav@hotmail.com>
2018-05-02 22:14:59 +0530

    while loop changed

--- random_driver.cpp
+++ random_driver.cpp
@@ -18,8 +18,8 @@
 #define ALPHA 10
 #define SMAX 5
 #define NO_SECTORS 360/ALPHA
-#define THRESHOLD 100
-#define SAFE_SECTOR_CNT (60/ALPHA)
+#define THRESHOLD 10
+#define SAFE_SECTOR_CNT 5
 
 
     geometry_msgs::Twist deep;              ///cmd_vel
@@ -31,7 +31,7 @@ std::vector<std::vector<double> > magnitude(20, std::vector<double>(20, 0));
 std::vector<double> h(NO_SECTORS);
 std::vector<double> candidateSectors;
 double curr_x=10,curr_y=10;
-double dest_x,dest_y;
+double dest_x=5,dest_y=5;
 
 //if  distance is not between 29-31 then it has obstacle
 double bot_position_x,bot_position_y;
@@ -41,34 +41,27 @@ double bot_position_x,bot_position_y;
    double theta=0.0;
    
 
-void turn(double angle){
-    if(cnt%10==0){
-    deep.angular.z=(angle);
-    cnt++;
-}
-    //pub.publish(msg);
-}
-
+//if  distance is not between 29-31 then it has obstacle
 double findx(double x,double y)
 {
           double x1=0,y1=0;
 
-  if(x>=0 && y>=0){
+  if(x>0 && y>0){
         x1=curr_x-y;
         y1=x+curr_y;
     }
 
-    else if(x<=0 && y<=0){
+    else if(x<0 && y<0){
         x1=(-1)*y+curr_x;
         y1=(-1)*x+curr_y;
     }
 
-    else if(x>=0 && y<=0){
+    else if(x>0 && y<0){
         x1=(-1)*y+curr_x;
         y1=x+curr_y;
     }
     
-    else if(x<=0 && y>=0){
+    else if(x<0 && y>0){
         y1=curr_y-x;
         x1=curr_x-y;
     }
@@ -79,96 +72,41 @@ double findx(double x,double y)
 double findy(double x,double y)
 {   double x1=0,y1=0;
 
-  if(x>=0 && y>=0){
+  if(x>0 && y>0){
       x1=curr_x-y;
       y1=x+curr_y;
   }
 
-  else if(x<=0 && y<=0){
+  else if(x<0 && y<0){
       x1=(-1)*y+curr_x;
       y1=(-1)*x+curr_y;
   }
 
-  else if(x>=0 && y<=0){
+  else if(x>0 && y<0){
       x1=(-1)*y+curr_x;
       y1=x+curr_y;
   }
   
-  else if(x<=0 && y>=0){
+  else if(x<0 && y>0){
       y1=curr_y-x;
       x1=curr_x-y;
   }
 
   return y1;
 }
-
+//update
 void maptomatrix(const sensor_msgs::LaserScan::ConstPtr& scan)  //to map value from polar to matrix
 {
-        // double x1=0,y1=0;
-
-
-        // for(int i = 0 ; i < scan->ranges.size(); i++){
-        //    // ROS_INFO("Value at angle %f is : %f", (i*scan->angle_increment+scan->angle_min)*180/PI, scan->ranges[i]);
-
-        //     if (scan->ranges[i]>29 && scan->ranges[i]<31){
-        //             //not an obstacle
-        //         }
-
-        //     else{
-        //         //this is obstacle
-        //         //find x and y coordinate for every range
-        //         //currrently at origin
-        //            //  double X=scan->ranges[i]*cos((135*PI/180)-(i*scan->angle_increment));
-        //            //  double Y=scan->ranges[i]*sin((135*PI/180)-(i*scan->angle_increment)); 
-        //            //  double x=X*cos(theta)-Y*sin(theta)+bot_position_x;
-        //            //  double y=X*sin(theta)+Y*cos(theta)+bot_position_y;  
-        //            //  std::cout<<"i :"<<i<<" scan range : "<<scan->ranges[i]<<"\n";
-        //            //  std::cout<<"Bot position x"<<bot_position_x<<" bot_position_y "<<bot_position_y<<"\n";
-        //              // std::cout<<"new coordinate X"<<X<<"New Y coordinate "<<Y<<"\n";
-        //              // std::cout<<"old coordinate X"<<x<<"old Y coordinate "<<y<<"\n";
-        //              // std::cout<<"=================================================\n";
-
-        //             // if(x>0 && y>0){
-        //             //     x1=curr_x-y;
-        //             //     y1=x+curr_y;
-        //             // }
-
-        //             // else if(x<0 && y<0){
-        //             //     x1=(-1)*y+curr_x;
-        //             //     y1=(-1)*x+curr_y;
-        //             // }
-
-        //             // else if(x>0 && y<0){
-        //             //     x1=(-1)*y+curr_x;
-        //             //     y1=x+curr_y;
-        //             // }
-                    
-        //             // else if(x<0 && y>0){
-        //             //     y1=curr_y-x;
-        //             //     x1=curr_x-y;
-        //             // }
-        //             // // y1 = curr_x + x;
-        //             // x1 = curr_y - y;
-
-        //             // std::cout<<"Now :"<<i<<" scan range : "<<scan->ranges[i]<<" x :"<<x1<<"\n";
-        //             // std::cout<<"y :"<<y1<<"\n";
-
-        //             histogramGrid[abs(x1)][abs(y1)]=1;
-
-        //     }
-    
-        // }
-
         double x1=0,y1=0;
         double sox=0;
         double soy=0;
 
 
         for(int i = 0 ; i < scan->ranges.size(); i++){
-            //ROS_INFO("Value at angle %f is : %f", (i*scan->angle_increment+scan->angle_min)*180/PI, scan->ranges[i]);
-            //std::cout<<"robot ---x"<<x<<"robot --y"<<y;
+            ROS_INFO("Value at angle %f is : %f", (i*scan->angle_increment+scan->angle_min)*180/PI, scan->ranges[i]);
+           // std::cout<<"robot ---x"<<x<<"robot --y"<<y;
 
-            //std::cout<<"robot x"<<findx(x,y)<<"robot y"<<findx(x,y);
+           // std::cout<<"robot x"<<findx(x,y)<<"robot y"<<findx(x,y);
 /*
             if(findx(x,y)==10 && findy(x,y)==10)
             {
@@ -202,9 +140,12 @@ void maptomatrix(const sensor_msgs::LaserScan::ConstPtr& scan)  //to map value f
                     // x1 = curr_y - y;
 
                      std::cout<<"Now after mapping :"<<" x :"<<x1<<"y :"<<y1<<"\n";
-                     if(x1>=0 && x1<20 && y1>=0 && y1<20)
-                    histogramGrid[abs(x1)][abs(y1)]=histogramGrid[abs(x1)][abs(y1)]+1;
 
+                   //std::cout<<"Now after mapping :"<<" x :"<<x1<<"y :"<<y1<<"\n";
+                     if(x1>=0 && x1<20 && y1>=0 && y1<20 && histogramGrid[abs(x1)][abs(y1)] < 5 
+                      && abs(x1) != bot_position_x && abs(y1) != bot_position_y)
+                      { histogramGrid[abs(x1)][abs(y1)]=histogramGrid[abs(x1)][abs(y1)]+1;
+                      }
 
             }
     
@@ -212,15 +153,12 @@ void maptomatrix(const sensor_msgs::LaserScan::ConstPtr& scan)  //to map value f
 
 
 
-
-
 }
 
-
 void findPolarObstacleDensity(){
-  // for(int i = 0 ; i < NO_SECTORS ; i++){
-  //  h[i] = 0.0;
-  // }
+  for(int i = 0 ; i < NO_SECTORS ; i++){
+    h[i] = 0.0;
+  }
   for(int i = 0 ; i < 20 ; i++){
     for(int j = 0 ; j < 20 ; j++){
       h[ (direction[i][j]/ALPHA) ] += magnitude[i][j] ;
@@ -283,7 +221,10 @@ void printPolarObstacleDensity(){
 }
 
 
+
+
 void directionMatrix(){
+
 
 // for(int i=0;i<20;i++){
 //   for(int j=0;j<20;j++){
@@ -315,6 +256,8 @@ void directionMatrix(){
       direction[i][j] = ang;
     } 
   }
+
+
 }
 
 double distance(double x1 ,double y1 ){
@@ -412,49 +355,41 @@ void printMagnitudeMatrix(){
 
 
 }
+
+
+
+
 void processLaserScan(const sensor_msgs::LaserScan::ConstPtr& scan){
     //Ranges is the unbounded array i.e. vector and it has size of 720
-   //   ROS_INFO("Printing....%lu", scan->ranges.size());
+      ROS_INFO("Printig....%lu", scan->ranges.size());
       //const nav_msgs :: Odometry::ConstPtr& odom;
       maptomatrix(scan);
       printMatrix();
       directionMatrix();
-      printDirectionMatrix();
+      //printDirectionMatrix();
       magnitudeMatrix();
-      printMagnitudeMatrix();
+      //printMagnitudeMatrix();
       findPolarObstacleDensity();
-      printPolarObstacleDensity();
+      //printPolarObstacleDensity();
 
-      double theta_dest = findTheta()*PI/180;
-      if(theta_dest==-1){
-        std::cout<<"Returned -1. Cannot turn!!\n";
-      }
-      else if(theta_dest > 180){
-        theta_dest = 360 - theta_dest;  
-       // turn(-theta_dest - theta);
-        std::cout<<"theta="<<-theta_dest<<" && "<<theta<<" => "<<(-theta_dest- theta)*180/PI <<"\n";
-      }else{
-       // turn(theta_dest - theta);
-        std::cout<<"theta="<<theta_dest<<" && "<<theta<<" => "<<(theta_dest - theta)*180/PI<<"\n";
-      }
-     
 }
 
 
 
-  
+   //Initializing the positions and z axis orientation
+
+   
 void odomCallback(const nav_msgs :: Odometry::ConstPtr& odom){
     //rostopic show Odometry
 
   x=odom->pose.pose.position.x;
   y=odom->pose.pose.position.y;
-    
- bot_position_x=findx(floor(x),floor(y));
-  bot_position_y=findy(floor(x),floor(y));
 
-std::cout<<"Bot position x"<<bot_position_x<<" bot_position_y "<<bot_position_y<<"\n";
-    std::cout<<"new coordinate X"<<x<<"New Y coordinate "<<y<<"\n";
 
+  //curr_x=x;
+  //curr_y=y;
+ bot_position_x=findx(x,y);
+  bot_position_y=findy(x,y);
 
   double quat_x=odom->pose.pose.orientation.x;
   double quat_y=odom->pose.pose.orientation.y;
@@ -480,6 +415,11 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh;
     ros::Subscriber scanSub;
 
+       std::cout<<"ENTER THE coordinates x and y:";
+    std::cin>>dest_x;
+    std::cin>>dest_y;
+
+
 
     //Ceates the publisher, and tells it to publish
     //to the husky_velocity_controller/cmd_vel topic, with a queue size of 100
@@ -489,47 +429,88 @@ int main(int argc, char **argv) {
     ros::Subscriber sub = nh.subscribe("/odometry/filtered", 10, odomCallback);
 
 
-    std::cout<<"ENTER THE coordinates x and y:";
-    std::cin>>dest_x;
-    std::cin>>dest_y;
-
-
-
     geometry_msgs::Point goal;              //rostopic show Odometry
     //say goal is (5,5)
     goal.x=5;
     goal.y=5;
 
+    geometry_msgs::Twist deep;              ///cmd_vel
 
     //Sets up the random number generator
     srand(time(0));
 
     //Sets the loop to publish at a rate of 10Hz
     ros::Rate rate(10);
-    //ros::spin();
-
       while(ros::ok()) {
 
-            // double inc_x=goal.x-x;
-            // double inc_y=goal.y-y;
+            double inc_x=goal.x-x;
+            double inc_y=goal.y-y;
             
             
-            // double angle_to_goal=atan2(inc_y,inc_x);        //atan2 returns in radians and it is inverse tan.
+            double angle_to_goal=atan2(inc_y,inc_x);        //atan2 returns in radians and it is inverse tan.
 
-            // if (abs(angle_to_goal - theta) > 0.1){          //if the vehicle is not facing to goal then change angle
-            //     deep.linear.x=0.0;
-            //     deep.angular.z=0.3;
-            // }
+            if (abs(angle_to_goal - theta) > 0.1){          //if the vehicle is not facing to goal then change angle
+                deep.linear.x=0.0;
+                deep.angular.z=0.3;
+            }
 
-            // else{                                           //if vehicle is facing to goal then go straight and dont change angle
-            //     deep.linear.x=0.5;
-            //     deep.angular.z=0.0;
+            else{                                           //if vehicle is facing to goal then go straight and dont change angle
+                deep.linear.x=0.5;
+                deep.angular.z=0.0;
 
-            // }
+            }
 
-             if(cnt%10!=0){
-                 deep.linear.x=2.0;
-                 cnt++;
+            //Publish the message
+            pub.publish(deep);
+            //Delays until it is time to send another message
+            rate.sleep();
+            ros::spinOnce();
+
+
+}
+/*
+ while(ros::ok()) {
+
+            //double inc_x=goal.x-x;
+            //double inc_y=goal.y-y;
+            
+            
+            //double angle_to_goal=atan2(inc_y,inc_x);        //atan2 returns in radians and it is inverse tan.
+
+
+            double theta_dest = findTheta()*PI/180;
+            double diff = 0.0;
+            std::cout<<"CHECK\n\n";
+        if(theta_dest==-1){
+          std::cout<<"Returned -1. Cannot turn!!\n";
+        }
+        else if(theta_dest > PI){
+          theta_dest = 2*PI - theta_dest; 
+          diff = -theta_dest - theta;
+        }else{
+          diff = theta_dest - theta;
+        }
+        std::cout<<"\ntheta="<<theta_dest<<" && "<<theta<<" => "<<(diff)*180/PI<<"\n";
+
+            if (diff > 0.05){          //if the vehicle is not facing to goal then change angle
+                deep.linear.x=0.0;
+                deep.angular.z=0.3;
+            }
+
+            else{                                           //if vehicle is facing to goal then go straight and dont change angle
+                deep.linear.x=5;
+                deep.angular.z=0.0;
+
+            }
+
+             // if(cnt%10!=0){
+             //     deep.linear.x=2.0;
+             //     cnt++;
+             // }
+
+             if((int)(bot_position_x) == (int)(dest_x) && (int)(bot_position_y) == (int)(dest_y)){
+              std::cout<<"REACHED!!!\n\n";
+              break;
              }
 
             //Publish the message
@@ -539,8 +520,7 @@ int main(int argc, char **argv) {
     ros::spinOnce();
 
 
-        }
-
+        }*/
 
 
 }
